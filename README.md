f7437f886d6137ebaf48c5b851c9ba08203a9396
Ramesh Nair <ram@hiddentao.com>
2016-04-29 17:39:38 +0800

    work towards tests

---
+++ README.md
@@ -0,0 +1,40 @@
+# Thinodium RethinkDB adapter
+
+[![Build Status](https://travis-ci.org/hiddentao/thinodium-rethinkdb.svg?branch=master)](http://travis-ci.org/hiddentao/thinodium-rethinkdb)
+
+A RethinkDB adapter for [thinodium](https://github.com/hiddentao/thinodium).
+
+Features:
+
+* Creates table and indexes if they don't already exist.
+
+## Installation
+
+This package must be used in conjunction with `thinodium`.
+
+```bash
+$ npm install thinodium thinodium-rethinkdb
+```
+
+## Examples
+
+```js
+
+```
+
+## Building
+
+To run the tests:
+
+    $ npm install
+    $ npm test
+
+## Contributing
+
+Contributions are welcome! Please see [CONTRIBUTING.md](https://github.com/hiddentao/thinodium-rethinkdb/blob/master/CONTRIBUTING.md).
+
+## License
+
+MIT - see [LICENSE.md](https://github.com/hiddentao/thinodium-rethinkdb/blob/master/LICENSE.md)
+
+
---
+++ index.js
@@ -0,0 +1,133 @@
+"use strict";
+
+const _ = require('lodash'),
+  Q = require('bluebird'),
+  Thinodium = require('thinodium'),
+  Model = Thinodium.Model;
+
+
+class RethinkDBModel extends Model {
+  /**
+   * Construct this model instance.
+   * 
+   * @param  {Object} db  Database connection object.
+   * @param  {String} name Table name.
+   * @param  {Object} [cfg] Configuration
+   * @param  {Object} [cfg.indexes] Indexes to setup.
+   */
+  constructor (db, name, cfg) {
+    cfg = cfg || {};
+
+    if (!cfg.pk) {
+      cfg.pk = 'id';
+    }
+
+    super(db, name, cfg);
+  }
+
+
+  /**
+   * @override
+   */
+  init () {
+    // get list of table
+    return this.db.tableList()
+      .then((tables) => {
+        // do we need to create our table?
+        if (0 > tables.indexOf(this.name)) {
+          return this.db.tableCreate(this.name, {
+            primaryKey: this.pk
+          });
+        }
+      })
+      .then(() => {
+        return this._qry().indexList();
+      })
+      .then((existingIndexes) => {
+        let promises = [];
+
+        for (let index of this._cfg.indexes) {
+          if (0 > existingIndexes.indexOf(index.name)) {
+            promises.push(this._qry().indexCreate(
+              index.name, index.def, index.options
+            ));
+          }
+        }
+
+        return promises;
+      });
+  }
+
+
+  /**
+   * @override
+   */
+  _qry () {
+    return this.db.table(this.name);
+  }
+
+
+  /**
+   * @override
+   */
+  _get (id) {
+    return Q.try(() => {
+      if (undefined === id || null === id) {
+        return null;
+      }
+
+      return this._qry().get(id).run()
+        .then((result) => {
+          return this._wrap(result);
+        });
+    });
+  }
+
+  /**
+   * @override
+   */
+  _insert (rawDoc) {
+    return Q.try(() => {
+      if (this.schema) {
+        this.schema.validate(rawDoc);
+      }
+
+      return this._qry().insert(rawDoc).run()
+        .then((ret) => {
+          let newDoc = _.extend({}, rawDoc);
+
+          if (!newDoc[this.pk] && ret.generated_keys) {
+            newDoc[this.pk] = ret.generated_keys[0];
+          }
+
+          return newDoc;
+        });
+    });
+  }
+
+  /**
+   * @override
+   */
+  _update (id, changes, document) {
+    return Q.try(() => {
+      if (this.schema) {
+        this.schema.validate(changes, {
+          ignoreMissing: true,
+        });        
+      }
+
+      return this._qry().get(id).update(changes).run();
+    });
+  }
+
+  /**
+   * @override
+   */
+  _remove (id) {
+    return this._qry().get(id).delete().run();
+  }
+
+};
+
+
+exports.Model = RethinkDBModel;
